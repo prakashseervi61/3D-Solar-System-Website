d83f310a0508245441a35b3ef5065fb22cd8adeb
Prakash V <prakashseervi1503@gmail.com>
2026-03-10 21:02:06 +0530

    Updates

---
+++ _next/static/3QuO6U69F5RM9e1-HzMP5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/3D-Solar-System-Website//_next/:path+",
+        "destination": "/3D-Solar-System-Website/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
